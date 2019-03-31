6a5221f04224080d0a2c7d4024b4becd8ba3fdfb
Lucas Santiago <santiagolucas643@gmail.com>
2019-03-31 16:51:59 -0300

    Rays completed.

--- CG.js
+++ CG.js
@@ -1,4 +1,3 @@
-
 let modo = 0;
 
 //------------Only-Polygons------------
@@ -15,7 +14,10 @@ let cor;
 
 //------------Only-Rays----------------
 
-let rays = [];
+let raios = [];
+
+let r1;
+let r2;
 
 function vertice(x, y){
     this.x = x;
@@ -27,17 +29,14 @@ function poligono(v, c){
     this.cor = c;
 }
 
-function ray(v){
-    this.vertice = v;
-    this.angulo = 0;
-    this.updateAngle = function(x, y){
-        this.angle = atan2((y - this.vertice.y)/(x - this.vertice.x));
+function raio(v){
+    this.v = v;
+    this.ang = 0;
+    this.updateAngle = function(px,py){
+        deltaY = (py - this.v.y);
+        deltaX = (px - this.v.x);
+        this.ang = atan2(deltaY,deltaX);
     }
-}
-
-function setup() {
-    createCanvas(400,400);
-    cor = color(randomColor(), randomColor(), randomColor(), 128);
 }
 
 function randomColor(){
@@ -47,25 +46,28 @@ function randomColor(){
 // Inserts position of mouse when pressed into vertex array.
 function mousePressed(){
     let v = new vertice(mouseX, mouseY);
-    vertices.push(v);
+    if (!modo) vertices.push(v);
+    else{
+        let r = new raio(v);
+        raios.push(r);
+    }
 }
 
 function mouseDragged(){
     if (modo){
-        let r = new ray(v);
-        r.updateAngle(mouseX, mouseY);
-        line(r.vertice.x, r.vertice.y, 10*cos(r.ang), 10*sin(r.ang));
-
-        let v1 = new vertice(15*cos(r.ang), 15*sin(r.ang));
-        let v2 = new vertice(10*cos(r.ang) - 5*sin(r.ang), 10*sin(r.ang) - 5*cos(r.ang));
-        let v3 = new vertice(10*cos(r.ang) + 5*sin(r.ang), 10*sin(r.ang) + 5*cos(r.ang));
-        triangle(v1.x,v1.y,v2.x,v2.y,v3.x,v3.y);
-        mouseReleased(r);    
+        let r = raios[raios.length-1];
+        r.updateAngle(mouseX,mouseY);
     }
 }
 
-function mouseReleased(ray){
-    if (modo) rays.push(ray);
+// Finishes creating a polygon.
+function doubleClicked(){
+    if (!modo){
+        let p = new poligono(vertices, cor);
+        poligonos.push(p);
+        vertices = [];
+        cor = color(randomColor(), randomColor(), randomColor(), 128);
+    }
 }
 
 // Switches modes between ray casting and polygon drawing.
@@ -75,14 +77,28 @@ function keyPressed(){
     vertices = [];
 }
 
-// Finishes creating a polygon.
-function doubleClicked(){
-    if (!modo){
-        let p = new poligono(vertices, cor);
-
-        poligonos.push(p);
-        vertices = [];
-        cor = color(randomColor(), randomColor(), randomColor(), 128);
+function desenharRaio(){
+    for (let r of raios){
+        push();
+        strokeWeight(5);
+        point(r.v.x, r.v.y);
+        pop();
+        
+        let lineEndX = r.v.x + 40*cos(r.ang);
+        let lineEndY = r.v.y + 40*sin(r.ang);
+          
+        let tempC = 2.5*cos(r.ang);
+        let tempS = 2.5*sin(r.ang);
+        
+        push();
+        strokeWeight(2);
+        line(r.v.x, r.v.y, lineEndX, lineEndY);
+        pop();  
+      
+        push();
+        fill(color(0));
+        triangle(lineEndX + 2*tempC, lineEndY + 2*tempS, lineEndX - tempS, lineEndY + tempC, lineEndX + tempS, lineEndY - tempC);
+        pop();
     }
 }
 
@@ -97,8 +113,9 @@ function desenharPoligono(){
     endShape(CLOSE);
 }
 
-function desenharRaio(){
-    
+function setup() {
+    createCanvas(400,400);
+    cor = color(randomColor(), randomColor(), randomColor(), 128);
 }
 
 // Keeps old polygons/rays on-screen.
@@ -109,19 +126,15 @@ function draw() {
         noFill();
         ellipse(mouseX, mouseY, 25, 25);
     }
-
     for (let poligono of poligonos){
         beginShape();
         let v = poligono.vertices;
-
         for(let p of v){
             vertex(p.x, p.y);
         }
-
         fill(poligono.cor);
         endShape(CLOSE);
     }
-
-    if (modo) desenharRaio();
-    else desenharPoligono();
+    desenharPoligono();
+    desenharRaio();
 }
